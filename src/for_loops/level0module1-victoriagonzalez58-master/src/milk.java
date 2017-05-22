2cc0468e5148b353a6103fc8dc9d213f3b635598
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-05-22 14:54:57 -0700

    Add files via upload

---
+++ src/for_loops/level0module1-victoriagonzalez58-master/src/milk.java
@@ -0,0 +1,14 @@
+
+public class milk {
+	public static void main(String[] args){
+	
+		
+		for(int i =99;i>=0;i = i-1){
+			System.out.print("99 bottles of milk on the wall, take one down pass it around " +i+" bottles of milk of milk on the wall n");
+			
+			
+			
+	}
+	}
+	}
+
