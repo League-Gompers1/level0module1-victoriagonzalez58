f8d6f7493e92973d6b4d22d84af1a2da35f3b6b2
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-07-24 14:58:51 -0700

    Add files via upload

---
+++ loopfour.java
@@ -0,0 +1,47 @@
+
+public class loopfour {
+public static void main(String[] args) {
+	for (int i = 0; i <= 100; i++) {
+		System.out.println(i);
+	}
+	for (int i = 100; i >= 0; i--) {
+		System.out.println(i);
+	}
+	for (int i = 2; i <= 100; i+=2) {
+		System.out.println(i);
+	}
+	for (int i = 1; i <= 99; i+=2) {
+		System.out.println(i);
+	}
+	for (int i = 1; i <= 500; i+=2) {
+		System.out.println(i + " odd");
+		System.out.println(i+1 + " even");
+	}
+	
+ for (int i = 7; i <=777; i+=7) {
+	System.out.println(i);
+}
+	for (int i = 2002; i <= 2017; i++) {
+		System.out.println("In "+i+ ", I was " + (i-2002) + " years old");
+	}
+	for (int i = 0; i <=2; i++) {
+		
+		for (int j = 0; j <=2 ; j++) {
+			System.out.println(i +" "+ j);
+		}
+	}
+	for (int i = 0; i <=7; i+=3) {	
+		
+		for (int j = 1; j <= 3; j++) {
+			
+			System.out.print(i+j+" ");
+
+	}
+	System.out.println(" ");
+			}
+
+	
+{
+}
+}
+}
