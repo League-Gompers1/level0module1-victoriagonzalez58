af88fb66d225aceee14fdcf182ca7c7b4d67a4fb
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-07-31 14:55:28 -0700

    Add files via upload
    not done

---
+++ src/for_loops/level0module1-victoriagonzalez58-master/src/for_loops/newairline.java
@@ -0,0 +1,14 @@
+package for_loops;
+
+import javax.swing.JOptionPane;
+
+public class newairline {
+	public static void main(String[] args) {
+		String ans = JOptionPane.showInputDialog(null, "First name");
+		
+		
+		
+		
+		
+}
+}
